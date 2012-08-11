c1014516bbacd2fc9be98738bfd6caf2c2a4eb8b
Michael Bemmerl <mail@mx-server.de>
2012-08-11 02:59:24 +0200

    Code for downloading a replication sequence.

---
+++ download-sequence.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PAD_SEQ=$(printf "%03d" $1)
+
+BASE_URL=http://planet.osm.org/redaction-period/day-replicate/000/000/
+OSC_URL=$BASE_URL/$PAD_SEQ.osc.gz
+STATE_URL=$BASE_URL/$PAD_SEQ.state.txt
+
+download ()
+{
+	FILE=$(basename $1)
+
+	# Delete file if existing	
+	if [ -e $FILE ]
+		then rm $FILE
+	fi
+	
+	curl --progress-bar -o $FILE $1
+}
+
+download $OSC_URL
+download $STATE_URL
