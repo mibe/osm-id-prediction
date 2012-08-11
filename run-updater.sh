530516a2d9b3c05eb911787085634cec0a9738a1
Michael Bemmerl <mail@mx-server.de>
2012-08-11 04:06:42 +0200

    Calling the database update script from run-updater.sh

--- run-updater.sh
+++ run-updater.sh
@@ -6,7 +6,7 @@ then
 fi
 
 LAST_SEQ=$(cat last-sequence)
-CURR_SEQ=$(exec ./get-latest-sequence.sh)
+CURR_SEQ=$(./get-latest-sequence.sh)
 
 echo "Latest sequence in database: $LAST_SEQ"
 echo "Current sequence on server: $CURR_SEQ"
@@ -21,6 +21,14 @@ COUNTER=$LAST_SEQ
 
 while [ $COUNTER -lt $CURR_SEQ ]
 do
+	# Replication sequences are one-based.
 	let COUNTER=COUNTER+1
 
+	echo "$COUNTER - Downloading replication sequence..."
+	./download-sequence.sh $COUNTER
+	echo "$COUNTER - Done downloading - detectig highest element ID..."
+	./update-db.sh $COUNTER
+	echo "$COUNTER - Done."
 done
+
+echo "--- DONE ---"
