c9995927aaeba9e3981e89efd0e6b5d2c04e0a06
Michael Bemmerl <mail@mx-server.de>
2012-09-09 04:44:58 +0200

    Commented plot.r

--- plot.r
+++ plot.r
@@ -1,3 +1,6 @@
+# Read node database; V1 = day, V2 = max. ID for this day
 nodes <- read.table("E:\\osm-replicates\\node.db", header=FALSE, sep=",")
+# Set V1 as date
 nodes$V1 <- as.Date(nodes$V1, "%Y-%m-%d")
+# Plot as line
 plot(V2 ~ V1, nodes, type = "l")
