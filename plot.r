c7426f9e84a46a542e45dfc6ea4728593035c65d
Michael Bemmerl <mail@mx-server.de>
2012-09-09 05:04:56 +0200

    Added regression line to plot.

--- plot.r
+++ plot.r
@@ -3,4 +3,5 @@ nodes <- read.table("E:\\osm-replicates\\node.db", header=FALSE, sep=",")
 # Set V1 as date
 nodes$V1 <- as.Date(nodes$V1, "%Y-%m-%d")
 # Plot as line
-plot(V2 ~ V1, nodes, type = "l")
+plot(V2 ~ V1, nodes, type = "l", yaxt = "n", xlab="Date", ylab="ID")
+axis(2, nodes$V2)
