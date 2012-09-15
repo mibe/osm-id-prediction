bc48d9a342566d52f43018a961d8ae98a99763f2
Michael Bemmerl <mail@mx-server.de>
2012-09-15 04:02:40 +0200

    Using column names & classes

--- plot.r
+++ plot.r
@@ -1,15 +1,16 @@
 # Read node database; V1 = day, V2 = max. ID for this day
-nodes <- read.table("E:\\osm-replicates\\node.db", header=FALSE, sep=",")
-# Set V1 as date
-nodes$V1 <- as.Date(nodes$V1, "%Y-%m-%d")
+nodes <- read.table("E:\\osm-replicates\\node.db", header=FALSE, sep=",",
+  colClasses=c("Date", "numeric"), col.names=c("day", "maxid"))
+attach(nodes)
+
 # Plot as line
-plot(V2 ~ V1, nodes, type = "l", yaxt = "n", xlab="Date", ylab="ID")
-axis(2, nodes$V2)
+plot(maxid ~ day, nodes, type = "l", yaxt = "n", xlab="Date", ylab="ID")
+axis(2, maxid)
 
 # Regression line
-reg = lm(nodes$V2 ~ nodes$V1)
+reg = lm(maxid ~ day)
 abline(reg, col="red")
 
 # LOWESS
-low = lowess(nodes$V2, nodes$V1)
+low = lowess(maxid, day)
 lines(low, col="blue")
