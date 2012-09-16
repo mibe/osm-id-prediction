2fb782dd3ec535b197107da7beb3d375d81a50ae
Michael Bemmerl <mail@mx-server.de>
2012-09-16 04:43:25 +0200

    Plot additional regression lines with last 30 & 90 days as data. A legend was also added. Lowess was commented out, that was too much.

--- plot.r
+++ plot.r
@@ -7,16 +7,28 @@ db <- read.table(path, header=FALSE, sep=",",
   colClasses=c("Date", "numeric"), col.names=c("day", "maxid"))
 attach(db)
 
-# Plot as line
+# Build linar regression model
+reg = lm(maxid ~ day)
+reg30 = lm(maxid ~ day, tail(db, n=30))
+reg90 = lm(maxid ~ day, tail(db, n=90))
+
+# Plot everything
 plot(maxid ~ day, db, type = "l", yaxt = "n", xlab="Date", ylab="ID")
 axis(2, maxid)
+axis.Date(3, day, format="%Y-%m-%d")
 
-# Regression line
-reg = lm(maxid ~ day)
+# Regression lines
 abline(reg, col="red")
+abline(reg30, col="blue")
+abline(reg90, col="green")
+
+# Legend
+legend(min(day), max(maxid),
+  c("highest ID per day", "all data", "last 30 days", "last 90 days"),
+  lty=c(1,1,1,1), lwd=c(2,2,2,2), col=c("black","red","blue","green"))
 
 # LOWESS
-low = lowess(db)
-lines(low, col="blue")
+#low = lowess(db)
+#lines(low, col="blue")
 
 detach(db)
