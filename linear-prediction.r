5ca67e02a2360daa1cc66dc8d425543f72e7adee
Michael Bemmerl <mail@mx-server.de>
2012-09-16 03:01:17 +0200

    Use only a subset of data for the calculation

--- linear-prediction.r
+++ linear-prediction.r
@@ -5,17 +5,21 @@ path <- "E:\\osm-replicates\\node.db"
 # 1st col. is the day, 2nd col. is the highest ID for this day
 db <- read.table(path, header=FALSE, sep=",",
   colClasses=c("Date", "numeric"), col.names=c("day", "maxid"))
-attach(db)
 
 # This is the prediction target (2^31)
 # MAXINT = (2^31)-1
 targetid <- 2147483648
 
+# Only use the data from the last 30 days for the
+# linear model
+db <- tail(db, n=30)
+
 # Create linear model
-model <- lm(day ~ maxid)
-summary(lm(maxid ~ day))
+model <- lm(day ~ maxid, db)
+summary(lm(maxid ~ day, db))
 
 # Calculate prediction
 prediction <- predict(model, data.frame(maxid = targetid))
-# Result is in days from 1970-01-01
+
+# Prediction result is in days from 1970-01-01
 as.Date(prediction, origin="1970-01-01")
